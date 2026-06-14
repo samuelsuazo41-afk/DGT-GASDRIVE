abf8c2d5852c5a822d9bab19b2ba848af671a429
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-14 15:41:18 +0200

    Update service worker cache version to v9.1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gasdrive-v8.5.1-es'; // subo versión para forzar update
+const CACHE = 'gasdrive-v9.1.1-es'; // subo versión para forzar update
 const FILES = [
   './',
   './index.html',
