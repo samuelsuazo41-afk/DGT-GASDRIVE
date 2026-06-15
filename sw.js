2bf69d815851f4f7ab844afa5d3800278d11a550
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-15 13:11:10 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gasdrive-v11.1.1-es'; // subo versión para forzar update
+const CACHE = 'gasdrive-v12.1.1-es'; // subo versión para forzar update
 const FILES = [
   './',
   './index.html',
