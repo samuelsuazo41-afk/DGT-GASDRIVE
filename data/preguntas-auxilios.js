3d90261fb9dda119e04313775a4fc6be2d73bdca
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-06-21 10:58:04 +0200

    Actualizar preguntas-auxilios.js

--- data/preguntas-auxilios.js
+++ data/preguntas-auxilios.js
@@ -1,4 +1,4 @@
- const PREGUNTAS_AUXILIOS= [
+const PREGUNTAS_AUXILIOS= [
 // === PROTOCOLO PAS === Pág 40-41   
 {q:"¿Qué haces primero ante un accidente?",a:["Llamar al 112","Proteger la zona","Socorrer heridos"],ok:1},
     {q:"Orden correcto protocolo PAS:",a:["Socorrer, Avisar, Proteger","Proteger, Avisar, Socorrer","Avisar, Proteger, Socorrer"],ok:1},
