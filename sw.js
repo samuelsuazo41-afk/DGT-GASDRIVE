e89e388488e072f70f9b32668c5ccab93c4b52c2
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-08-31 16:01:18 +0200

    Increment cache version to v10.7.5
    Updated cache version to v10.7.5 in service worker.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gasdrive-v10.7.4'; // SUBE ESTA VERSION CADA VEZ QUE CAMBIES ALGO
+const CACHE = 'gasdrive-v10.7.5'; // SUBE ESTA VERSION CADA VEZ QUE CAMBIES ALGO
 const FILES = [
   './',
   './index.html',
